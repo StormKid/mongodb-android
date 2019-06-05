5afc8b426912dbe7c3383810ba0138c393de580e
ke_li <stokid@126.com>
2019-06-05 14:56:43 +0800

    init sample

--- app/src/main/java/com/stormkid/example/MainActivity.kt
+++ app/src/main/java/com/stormkid/example/MainActivity.kt
@@ -2,11 +2,27 @@ package com.stormkid.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.stormkid.mongokt.Config
+import com.stormkid.mongokt.Model
+import com.stormkid.mongokt.curd.ActionDocument
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       val collection =  Config.instance.Builder().build()
+       val action = ActionDocument(collection!!).deal(Model.InportBean(Entity("demo")))
+
+
+        action.findAll()
+        action.insert(false)
+        action.replace(Model.InportBean(Entity("other")))
+        action.delete()
     }
+
+
+     data class Entity(val name:String):Model.BaseEntity()
+
+
 }
